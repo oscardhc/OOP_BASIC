425b49a0d5786383898a65900fd2416da141a215
oscardhc <oscardhc@sjtu.edu.cn>
2018-11-20 14:50:40 +0800

    Initial Commit

---
+++ OOP_BASIC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_BASIC
+//
+//  Created by Haichen Dong on 2018/11/20.
+//  Copyright © 2018 Haichen Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
